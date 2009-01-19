28bbffd743dac8b81d4e7f9c19c595b8fee85c01
Tobias Ebnöther <Ar@gorn.ch>
2009-01-19 10:35:22 +0100

    First basic version

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-
-
-
+import os
 import wsgiref.handlers
-
-
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
 
-
-class MainHandler(webapp.RequestHandler):
-
+class MainPage(webapp.RequestHandler):
   def get(self):
-    self.response.out.write('Hello world!')
+    user = users.get_current_user()
+    if not user:
+        path = users.create_login_url("/")
+        self.redirect(path)
+        return
+        
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, user))
 
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
-                                       debug=True)
-  wsgiref.handlers.CGIHandler().run(application)
+  run_wsgi_app(application)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
   main()
