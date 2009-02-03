f3498ab6f5358b3914fb01cb95391e10905f094a
Tobias Ebnöther <Ar@gorn.ch>
2009-02-03 22:20:57 +0100

    Fixed basic modeling

--- models.py
+++ models.py
@@ -1,24 +1,28 @@
 from google.appengine.ext import db
 
+class Group(db.Model):
+	name = db.StringProperty()
+	creation = db.DateTimeProperty(auto_now_add=True)
+	owner = db.UserProperty()
+
+class GroupMember(db.Model):
+	group = db.ReferenceProperty(Group)
+	member = db.UserProperty()
+	creation = db.DateTimeProperty(auto_now_add=True)
+
 class Task(db.Model):
 	owner = db.UserProperty()
 	creation = db.DateTimeProperty(auto_now_add=True)
 	description = db.StringProperty()
 	jira = db.StringProperty()
 	time = db.FloatProperty()
+	estimateCount = db.IntegerProperty()
+	estimateAverage = db.FloatProperty()
+	group = db.ReferenceProperty(Group)
 
 class Estimation(db.Model):
 	taskId = db.ReferenceProperty(Task)
 	owner = db.UserProperty()
 	creation = db.DateTimeProperty(auto_now_add=True)
 	time = db.FloatProperty()
-	
-class Group(db.Model):
-	name = db.StringProperty()
-	creation = db.DateTimeProperty(auto_now_add=True)
-	owner = db.UserProperty()
-	
-class GroupMember(db.Model):
-	group = db.ReferenceProperty(Group)
-	member = db.UserProperty()
-	creation = db.DateTimeProperty(auto_now_add=True)
+
