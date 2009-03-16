ae8b22f9108647e7cf46ff38a2da2169c4b020a2
Tobias Ebnöther <Ar@gorn.ch>
2009-03-16 21:37:26 +0100

    Basic request display done

--- modules/groups.py
+++ modules/groups.py
@@ -11,11 +11,23 @@ class Overview(object):
 	def getOwnGroups(self):
 		return models.GroupMember.gql("WHERE name = :1 ORDER BY creation DESC", users.get_current_user())
 	
+	def getMemberRequests(self, members):
+		requests = []
+		for member in members:
+			if 2 == member.status :
+				reqs = models.GroupMember.gql('WHERE group = :1 AND status = 0', member.group)
+				for request in reqs:
+					requests.append(request)
+		return requests
+	
 	def process(self, request):
+		ownGroups = self.getOwnGroups()
 		return {
 			'groups': self.getGroups(),
-			'member': self.getOwnGroups(),
+			'member': ownGroups,
+			'requests': self.getMemberRequests(ownGroups)
 		}
+	
 
 class Add(object):
 	error = False
@@ -58,7 +70,7 @@ class Add(object):
 class Join(object):
 	error = False
 	message = ''
-
+	
 	def process(self, request, groupKey):
 		try :
 			group = db.get(groupKey);
@@ -98,9 +110,12 @@ class Join(object):
 					member.put()
 					self.message = 'Requested to join group.'
 		overview = Overview()
+		ownGroups = overview.getOwnGroups()
 		return {
 			'groups': overview.getGroups(),
-			'member': overview.getOwnGroups(),
+			'member': ownGroups,
+			'requests': overview.getMemberRequests(ownGroups),
 			'error': self.error,
 			'message': self.message
 		}
+	
