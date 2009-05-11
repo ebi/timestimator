c837bc050d7eebcc773b99c56defa94d92b77374
Tobias Ebnöther <Ar@gorn.ch>
2009-05-11 15:53:54 +0200

    Sending email when requesting group joins

--- modules/groups/join.py
+++ modules/groups/join.py
@@ -1,7 +1,7 @@
 import models
 from google.appengine.ext import db
 from google.appengine.api import users
-
+from google.appengine.api import mail
 
 class Join(object):
 	error = False
@@ -25,7 +25,12 @@ class Join(object):
 					if 'True' == request.get('setAdmin'):
 						self.message = 'Admin privileges granted.'
 						member.status = 2
-					else: 
+					else:
+						user = users.get_current_user()
+						sender_address = user.email()
+						subject = '[timestimator] request accepted'
+						body = """%s has accepted your request to join the group %s.""" % (user.nickname(), group.name)
+						mail.send_mail(sender_address, member.name.email(), subject, body)
 						self.message = 'Join request accepted.'
 						member.status = 1;
 					member.put()
@@ -38,12 +43,19 @@ class Join(object):
 					self.error = True
 					self.message = 'Already member of this group.'
 				else:
+					user = users.get_current_user()
 					member = models.GroupMember(
 						group = group.key(),
-						name = users.get_current_user(),
+						name = user,
 						status = 0,
 					)
 					member.put()
+					sender_address = user.email()
+					subject = '[timestimator] ' + user.nickname() + 'requested to join ' + group.name
+					body = """%s requested to join your group %s.
+Visit http://timestimator.gorn.ch/groups to accept or deny the request.
+					""" % (user.nickname(), group.name)
+					mail.send_mail(sender_address, group.owner.email(), subject, body)
 					self.message = 'Requested to join group.'
 		return {
 			'error': self.error,
