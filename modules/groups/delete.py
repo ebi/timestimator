fb97ac45720ebb02ebb62b3bef0d6efbd62b7201
Tobias Ebnöther <Ar@gorn.ch>
2009-05-11 10:02:42 +0200

    Deleting feature added

--- modules/groups/delete.py
+++ modules/groups/delete.py
@@ -1,4 +1,5 @@
 import models
+import helper
 from google.appengine.ext import db
 from google.appengine.api import users
 
@@ -6,7 +7,25 @@ class Delete(object):
 	error = False
 	message = ''
 	
-	def process(self):
+	def process(self, request, groupKey):
+		if request.get('delete') != groupKey:
+			self.error = True
+			self.message = "GroupId in body doesn't match URL"
+			
+		group = helper.getGroup(groupKey)
+		if False == group:
+			self.error = True
+			self.message = 'Invalid group. '
+		
+		user = helper.getUser(user)
+		if False == user:
+			self. error = True
+			self.message = 'User not found'
+		
+		if not helper.groupeStateWritePermission(user, group):
+			self.error = True
+			self.message = 'No permission to change this group. '
+			
 		return {
 			'error': self.error,
 			'message': self.message,
--- modules/groups/helper.py
+++ modules/groups/helper.py
@@ -2,20 +2,48 @@ import types
 import models
 from google.appengine.ext import db
 
-def isUserInGroup(user, group):
-	if types.StringType == type(user):
-		user = users.User(user)
-	
+def getGroup(group):
 	if types.StringType == type(group):
 		try:
-			print group
-			group = db.get(group)
+			return db.get(group)
 		except:
 			return False
+	else:
+		return group
+
+def getUser(user):
+	if types.StringType == type(user):
+		return users.User(user)
+	else:
+		return user
+	
+
+def isUserInGroup(user, group):
+	user = getUser(user)
+	group = getGroup(group)
 		
 	permissions = models.GroupMember.gql('WHERE group = :1 AND name = :2 and status > 0', group, user)
 	if permissions.count() > 0:
 		return True
 	else:
 		return False
+		
+def isUserAdmin(user, group):
+	user = getUser(user)
+	group = getGroup(group)
+	
+	permissions = models.GroupMember.gql('WHERE group = :1 AND name = :2 and status = 2', group, user)
+	if permissions.count() > 0:
+		return True
+	else:
+		return False
+
+def groupeStateWritePermission(user, group):
+	user = getUser(user)
+	group = getGroup(group)
+	
+	if isUserInGroup(user, group):
+		return True
+	else:
+		return isUserAdmin(user, group)
 	
